0f2989d15ea1fa4270599231e7e4f8c81147924c
AndrewSA123 <andrewknott_123@hotmail.co.uk>
2018-10-23 16:31:50 +0100

    Database stuff

---
+++ INSERTScriptMovieDB.sql
@@ -0,0 +1,12 @@
+USE moviedb;
+SELECT * FROM actors;
+SELECT * FROM movie;
+SELECT * FROM starredIn;
+INSERT INTO actors (name, dob) VALUES("Steven Amell", "1981/05/08");
+INSERT INTO actors (name, dob) VALUES("Grant Gustin", "1990/01/14");
+INSERT INTO actors (name, dob) VALUES("Emily Rickards", "1991/07/24");
+INSERT INTO actors (name, dob) VALUES("David Ramsey", "1971/11/17");
+INSERT INTO actors (name, dob) VALUES("Colton Haynes", "1988/07/13");
+INSERT INTO actors (name, dob) VALUES("Carlos Valdes", "1989/05/20");
+INSERT INTO movie (title) VALUES("Arrow");
+

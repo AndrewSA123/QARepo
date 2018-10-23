76b2c673f4226dbfd411646c56642e02aef9153e
AndrewSA123 <andrewknott_123@hotmail.co.uk>
2018-10-23 17:12:57 +0100

    Updating database stuff

--- INSERTScriptMovieDB.sql
+++ INSERTScriptMovieDB.sql
@@ -2,11 +2,18 @@ USE moviedb;
 SELECT * FROM actors;
 SELECT * FROM movie;
 SELECT * FROM starredIn;
+SELECT * FROM genre;
 INSERT INTO actors (name, dob) VALUES("Steven Amell", "1981/05/08");
 INSERT INTO actors (name, dob) VALUES("Grant Gustin", "1990/01/14");
 INSERT INTO actors (name, dob) VALUES("Emily Rickards", "1991/07/24");
 INSERT INTO actors (name, dob) VALUES("David Ramsey", "1971/11/17");
 INSERT INTO actors (name, dob) VALUES("Colton Haynes", "1988/07/13");
 INSERT INTO actors (name, dob) VALUES("Carlos Valdes", "1989/05/20");
+INSERT INTO movie (title) VALUES("Iron Man");
 INSERT INTO movie (title) VALUES("Arrow");
+INSERT INTO movie (title) VALUES("The Flash");
+INSERT INTO movie (title) VALUES("Doctor Strange");
+INSERT INTO movie (title) VALUES("DC's Legends of Tommorrow");
+
+INSERT INTO genre (genreName) VALUES("Action");
 
